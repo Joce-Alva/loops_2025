d566253f645723030f264a653f5019650dac57bb
Joce-Alva <jocealvarez8002@gmail.com>
2025-12-11 20:37:10 +0000

    loops

--- 4_problem_set_1.py
+++ 4_problem_set_1.py
@@ -40,7 +40,13 @@ print (factorial(n1))
 # ### **Problem 5: Print Even Numbers**
 
 # Ask the user for a number **n**, then print all **even numbers** from 2 up to n.
-
+n = int(input("Enter a number: "))
+list2 = list(range(2,n))
+for i in list2:
+    if i % 2 == 0:
+        print("even",i)
+    else:
+        print("odd",i)
 
 
 # ### **Problem 6: Reverse a String**
